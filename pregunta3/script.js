53bf5f099d267ddfdf7416726fe63a07082bf277
Melissa Nicolle Toledo Rodriguez <melitoledo5@gmail.com>
2021-02-23 21:15:41 -0600

    parcial

---
+++ pregunta3/script.js
@@ -0,0 +1,9 @@
+function registerNewUser() {
+    var user = document.getElementById("user").value;
+    var passw = document.getElementById("passw").value;
+    var rol = document.getElementById("rol").value;
+
+    alert("Se añadio" + user + "," + passw + "," + rol + "a nuestra tabla")
+}
+
+
